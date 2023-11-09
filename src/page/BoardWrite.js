774c2f688c9c6216493c1a92dae5b2d4b854a769
Nighteast <jung946244@naver.com>
2023-11-09 14:08:24 +0900

    성공 실패시 toast

--- src/page/BoardWrite.js
+++ src/page/BoardWrite.js
@@ -6,6 +6,7 @@ import {
   FormLabel,
   Input,
   Textarea,
+  useToast,
 } from "@chakra-ui/react";
 import axios from "axios";
 
@@ -14,6 +15,8 @@ export function BoardWrite() {
   const [content, setContent] = useState("");
   const [writer, setWriter] = useState("");
 
+  const toast = useToast();
+
   function handleSubmit() {
     axios
       .post("/api/board/add", {
@@ -21,8 +24,18 @@ export function BoardWrite() {
         content,
         writer,
       })
-      .then(() => console.log("잘됨"))
-      .catch(() => console.log("안됨"))
+      .then(() => {
+        toast({
+          description: "새 글이 저장되었습니다.",
+          status: "success",
+        });
+      })
+      .catch(() => {
+        toast({
+          description: "저장 중에 문제가 발생하였습니다.",
+          status: "error",
+        });
+      })
       .finally(() => console.log("끝"));
   }
 
@@ -48,7 +61,7 @@ export function BoardWrite() {
             onChange={(e) => setWriter(e.target.value)}
           ></Input>
         </FormControl>
-        <Button onClick={handleSubmit} colorScheme="facebook">
+        <Button onClick={handleSubmit} colorScheme="blue">
           저장
         </Button>
       </Box>
