db2bff453bf354ba7042e811ca0844d194c2dfac
Nighteast <jung946244@naver.com>
2023-11-09 15:28:35 +0900

    BoardList 각 데이터 행에 key prop 추가

--- src/page/BoardList.js
+++ src/page/BoardList.js
@@ -1,86 +1,53 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Box,
-  Button,
-  FormControl,
-  FormLabel,
-  Input,
-  Textarea,
-  useToast,
+  Spinner,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
 } from "@chakra-ui/react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
-export function BoardWrite() {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  const [writer, setWriter] = useState("");
-  const [isSubmitting, setIsSubmitting] = useState(false);
+export function BoardList() {
+  const [boardList, setBoardList] = useState(null);
 
-  const toast = useToast();
-  const navigate = useNavigate();
-
-  function handleSubmit() {
-    setIsSubmitting(true);
+  useEffect(() => {
     axios
-      .post("/api/board/add", {
-        title,
-        content,
-        writer,
-      })
-      .then(() => {
-        toast({
-          description: "새 글이 저장되었습니다.",
-          status: "success",
-        });
-        navigate("/");
-      })
-      .catch((error) => {
-        console.log(error.response.status);
-        if (error.response.status === 400) {
-          toast({
-            description: "작성한 내용을 확인해주세요.",
-            status: "error",
-          });
-        } else {
-          toast({
-            description: "저장 중에 문제가 발생하였습니다.",
-            status: "error",
-          });
-        }
-      })
-      .finally(() => setIsSubmitting(false));
-  }
+      .get("/api/board/list")
+      .then((response) => setBoardList(response.data));
+  }, []);
 
   return (
     <Box>
-      <h1>게시물 작성</h1>
+      <h1>게시물 목록</h1>
       <Box>
-        <FormControl>
-          <FormLabel>제목</FormLabel>
-          <Input value={title} onChange={(e) => setTitle(e.target.value)} />
-        </FormControl>
-        <FormControl>
-          <FormLabel>본문</FormLabel>
-          <Textarea
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          ></Textarea>
-        </FormControl>
-        <FormControl>
-          <FormLabel>작성자</FormLabel>
-          <Input
-            value={writer}
-            onChange={(e) => setWriter(e.target.value)}
-          ></Input>
-        </FormControl>
-        <Button
-          isDisabled={isSubmitting}
-          onClick={handleSubmit}
-          colorScheme="blue"
-        >
-          저장
-        </Button>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>id</Th>
+              <Th>title</Th>
+              <Th>by</Th>
+              <Th>at</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {boardList === null ? (
+              <Spinner />
+            ) : (
+              boardList.map((board) => (
+                <Tr key={board.id}>
+                  <Td>{board.id}</Td>
+                  <Td>{board.title}</Td>
+                  <Td>{board.writer}</Td>
+                  <Td>{board.inserted}</Td>
+                </Tr>
+              ))
+            )}
+          </Tbody>
+        </Table>
       </Box>
     </Box>
   );
