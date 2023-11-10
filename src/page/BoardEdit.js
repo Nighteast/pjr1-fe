3ffa7f98c83c0af47de939f18dd0913c9d9f5b68
Nighteast <jung946244@naver.com>
2023-11-10 09:46:48 +0900

    수정 페이지 내용 입력 가능하게 적용 (useImmer, handle함수) arrow function

--- src/page/BoardEdit.js
+++ src/page/BoardEdit.js
@@ -12,9 +12,10 @@ import React, { useEffect } from "react";
 import axios from "axios";
 
 export function BoardEdit() {
+  // useImmer 사용, 상태를 생성
   const [board, updateBoard] = useImmer(null);
 
-  // /edit/:id 이므로 useParams로 경로에서 가져옴
+  // /edit/:id 이므로 useParams로 URL에서 게시글 ID가져오기
   const { id } = useParams();
 
   useEffect(() => {
@@ -23,24 +24,54 @@ export function BoardEdit() {
       .then((response) => updateBoard(response.data));
   }, []);
 
+  // 게시글을 로딩중이라면 스피너 돌리기
   if (board === null) {
     return <Spinner />;
   }
+
+  // 보드 업데이트 함수로 useImmer update사용
+  //
+  const handleBoardUpdate = (e) => {
+    updateBoard((draft) => {
+      draft.field = e.target.value;
+    });
+  };
 
   return (
     <Box>
       <h1>{id}번 글 수정</h1>
       <FormControl>
         <FormLabel>제목</FormLabel>
-        <Input value={board.title} />
+        <Input
+          value={board.title}
+          onChange={(e) => {
+            updateBoard((draft) => {
+              draft.title = e.target.value;
+            });
+          }}
+        />
       </FormControl>
       <FormControl>
         <FormLabel>본문</FormLabel>
-        <Textarea value={board.content} />
+        <Textarea
+          value={board.content}
+          onChange={(e) => {
+            updateBoard((draft) => {
+              draft.content = e.target.value;
+            });
+          }}
+        />
       </FormControl>
       <FormControl>
         <FormLabel>작성자</FormLabel>
-        <Input value={board.writer} />
+        <Input
+          value={board.writer}
+          onChange={(e) => {
+            updateBoard((draft) => {
+              draft.writer = e.target.value;
+            });
+          }}
+        />
       </FormControl>
     </Box>
   );
