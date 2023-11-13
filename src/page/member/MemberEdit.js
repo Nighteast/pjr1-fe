7527edbb9b1573b89f40cf9f24ed9849d94f4e08
Nighteast <jung946244@naver.com>
2023-11-13 16:34:56 +0900

    회원 정보 수정 기능 - 수정 버튼 isDisabled={!emailChecked || !passwordChecked || !nickNameChecked} 추가

--- src/page/member/MemberEdit.js
+++ src/page/member/MemberEdit.js
@@ -219,7 +219,7 @@ export function MemberEdit() {
       </FormControl>
 
       <Button
-        isDisabled={!emailChecked || !passwordChecked}
+        isDisabled={!emailChecked || !passwordChecked || !nickNameChecked}
         colorScheme="blue"
         onClick={onOpen}
       >
