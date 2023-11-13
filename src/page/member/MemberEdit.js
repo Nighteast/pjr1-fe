a3325d7784a0c7ad720e2b89f090162f55de3179
Nighteast <jung946244@naver.com>
2023-11-13 12:38:23 +0900

    수정 후 성공(then), 오류(catch), 최종(finally) 코드 추가

--- src/page/member/MemberEdit.js
+++ src/page/member/MemberEdit.js
@@ -1,5 +1,5 @@
-import { useSearchParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import {
   Box,
@@ -8,7 +8,15 @@ import {
   FormControl,
   FormLabel,
   Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Spinner,
+  useDisclosure,
   useToast,
 } from "@chakra-ui/react";
 
@@ -21,6 +29,8 @@ export function MemberEdit() {
 
   const toast = useToast();
   const [params] = useSearchParams();
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios.get("/api/member?" + params.toString()).then((response) => {
@@ -80,10 +90,33 @@ export function MemberEdit() {
       });
   }
 
+  // 수정 버튼 클릭시 요청, 성공, 오류, 최종
   function handleSubmit() {
     // put /api/member/edit {id, password, email}
 
-    axios.put("/api/member/edit", { id: member.id, password, email });
+    axios
+      .put("/api/member/edit", { id: member.id, password, email })
+      .then(() => {
+        toast({
+          description: "회원정보가 수정되었습니다.",
+          status: "success",
+        });
+        navigate("/member?" + params.toString());
+      })
+      .catch((error) => {
+        if (error.response.status === 401 || error.response.status === 403) {
+          toast({
+            description: "수정 권한이 없습니다.",
+            status: "error",
+          });
+        } else {
+          toast({
+            description: "수정 중에 문제가 발생하였습니다.",
+            status: "error",
+          });
+        }
+      })
+      .finally(() => onClose());
   }
 
   return (
@@ -131,10 +164,26 @@ export function MemberEdit() {
       <Button
         isDisabled={!emailChecked || !passwordChecked}
         colorScheme="blue"
-        onClick={handleSubmit}
+        onClick={onOpen}
       >
         수정
       </Button>
+
+      {/* 수정 모달 */}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>수정 확인</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>수정 하시겠습니까?</ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>닫기</Button>
+            <Button onClick={handleSubmit} colorScheme="red">
+              수정
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
